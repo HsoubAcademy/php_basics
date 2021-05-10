12193dab065794a48b7de7fad8afdd4951f8126c
abdullatifey <53393897+abdullatifey@users.noreply.github.com>
2021-05-10 10:16:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,17 @@
 <?php
 
+use App\Controllers\TaskController;
+use App\Core\Request;
+use App\Core\Router;
 
-    print_r($array);
-    $value = $array[$key];
+require '_init.php';
 
-    var_dump($obj);
-    $obj->drinkWater();
-
-
-    exit;
-    $value = $array[$key];
-
-    die();
-    var_dump($obj);
-    $obj->drinkWater();
-
-
-    someCode;
-
-// count
-echo count(['Hello', 'World']);
-
-// is_array
-echo (integer) is_array([1, 2, 3]);
-echo (integer) is_array('hello');
-
-// explode
-$string = 'php javascript ruby swift kotlin';
-$arr = explode(' ', $string);
-print_r($arr);
-
-// implode
-echo implode('-', $arr);
-
-// array_merge
-$arr1 = ['red', 'green', 'blue'];
-$arr2 = ['black', 'yellow'];
-$result = array_merge($arr1, $arr2);
-print_r($result);
+Router::make()
+    ->get('/', [TaskController::class, 'index'])
+    ->post('/create', [TaskController::class, 'store'])
+    ->get('/update', [TaskController::class, 'update'])
+    ->get('/delete', [TaskController::class, 'delete'])
+    ->resolve(
+        Request::uri(),
+        Request::method()
+    );
